ceda80c972dd9efc8e20e196674ca952c1fcc7d2
Unknown <np.carlos@gmail.com>
2018-09-01 10:17:13 -0500

    CORS update

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module ApiGlobal
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+	
+	config.middleware.insert_before 0, Rack::Cors do
+		allow do
+			origins '*'
+			resource '*', :methods => [:get, :post, :options, :delete, :put, :patch]
+		end
+	end
   end
 end
